1576caa83f5d72cd54e53b9c76b390126c6ef79d
Brandon <brandonrodriguezvega@gmail.com>
2023-01-29 20:08:27 -0500

    Added components folder with different section for pages on website and some code on about me file

---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="top-title">Brandon Rodriguez</h2>
+            <hr></hr>
+            <div>
+            <img src="./images/808963A3-350F-4963-BABC-C6C8A0565633.jpg" alt="Picture of Brandon Rodriguez(myself)"/>
+                <p>
+                Georgia Tech Full Stack Bootcamp student with great promise, great work ethic, and incredible determination.
+                My goal after the bootcamp is to learn another language, hone my skills more, and hopefully lanch a career
+                in full stack development.
+                I am from Marietta,Georgia but I am currently living in Powder Springs. I am working with DPR Constuction as 
+                a Field Engineer. I am bilingual I speak english and spanish. I am a great team play, I pick up on things quick and
+                I have a hunger for knowledge
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <footer className="footer bg-dark fixed-bottom">
+        <a href="https://github.com/frutaseca"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="My Github" className="icon"/></a>
+        <a href="https://www.linkedin.com/in/brandon-rodriguez-990b98242/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt=" My LinkedIn" className="icon"/></a>
+    </footer>
+    )
+}
+
+export default Footer;
